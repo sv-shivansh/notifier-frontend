3ef118a523bc5a71934771dc7c2257f0e150b3d0
sv-shivansh <sv_shivansh@outlook.com>
2021-06-11 23:40:55 +0530

    added templates

--- slotform/views.py
+++ slotform/views.py
@@ -8,18 +8,16 @@ from .models import user
 
 # Create your views here.
 def home(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            district = request.POST["district_id"]
-            email = request.POST["email"]
-            telegram = request.POST["telegram"]
-            createUser = user(email=email, district_id=district, telegram_sub=telegram)
-            createUser.save()
-            return render(request, 'success.html', {'district':district, 'email':email, 'telegram':telegram})
-        return render(request, 'home.html')
-    else:
-        return redirect('login')
+    if request.method == 'POST':
+        district = request.POST["district_id"]
+        email = request.POST["email"]
+        telegram = request.POST["telegram"]
+        createUser = user(email=email, district_id=district, telegram_sub=telegram)
+        createUser.save()
+        return render(request, 'success.html', {'district':district, 'email':email, 'telegram':telegram})
+    return render(request, 'home.html')
 
+@login_required
 def register(request):
     if request.method == 'POST':
         # Get the post parameters
@@ -54,14 +52,16 @@ def register(request):
         myuser.save()
         messages.success(request, "Successfully, Registered.")
         return redirect('login')
-    return render(request, 'register.html')
+    if str(request.user) == 'shivansh':
+        return render(request, 'register.html')
+    return redirect('home')
 
 
 def login(request):
     if request.method == 'POST':
-        loginusername = request.POST['loginusername']
-        loginpassword = request.POST['loginpassword']
-        user = authenticate(username=loginusername, password=loginpassword)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
 
         if user is not None:
             auth_login(request, user)
