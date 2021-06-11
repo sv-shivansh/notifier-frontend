7369b3c53cf74fa51a6a860a88689364d34b559c
sv-shivansh <sv_shivansh@outlook.com>
2021-06-11 12:30:37 +0530

    timefield

--- slotform/models.py
+++ slotform/models.py
@@ -1,8 +1,8 @@
-from djongo import models
+from django.db import models
 
 # Create your models here.
 class user(models.Model):
     district_id = models.IntegerField()
     email = models.EmailField(max_length=254)
     telegram_sub = models.BooleanField(default=False)
-    time = models.TimeField(auto_now= True)
+    created_at = models.DateTimeField(auto_now_add=True)
