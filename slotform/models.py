640cc562afac50816b5a494439ba867f8132bb78
sv-shivansh <sv_shivansh@outlook.com>
2021-06-12 22:19:10 +0530

    improved flow

--- slotform/models.py
+++ slotform/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db.models.deletion import CASCADE
+from djongo import models
 
 # Create your models here.
 class user(models.Model):
@@ -6,3 +7,25 @@ class user(models.Model):
     email = models.EmailField(max_length=254)
     telegram_sub = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = "user"
+
+class state(models.Model):
+    _id = models.ObjectIdField()
+    state_id = models.IntegerField(unique=True)
+    state_name = models.TextField()
+    class Meta:
+        db_table = "states"
+
+class district(models.Model):
+    _id = models.ObjectIdField()
+    is_active = models.BooleanField()
+    is_telegram = models.BooleanField()
+    district_id = models.IntegerField()
+    district_name = models.TextField()
+    telegram_channel_name = models.TextField(default= '')
+    telegram_channel_id = models.TextField(default= '')
+    # state_id = models.ForeignObject(to=state, on_delete=models.CASCADE, from_fields=['state_id'], to_fields=['_id'])
+
+    class Meta:
+        db_table = "districts"
--- slotform/views.py
+++ slotform/views.py
@@ -4,73 +4,65 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth import logout, authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
-from .models import user
+from .models import user, state, district
 
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
-        district = request.POST["district_id"]
-        email = request.POST["email"]
-        telegram = request.POST["telegram"]
-        createUser = user(email=email, district_id=district, telegram_sub=telegram)
-        createUser.save()
-        return render(request, 'success.html', {'district':district, 'email':email, 'telegram':telegram})
-    return render(request, 'home.html')
-
-@login_required
-def register(request):
-    if request.method == 'POST':
-        # Get the post parameters
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        # Check for errors
-        if not username.isalnum():
-            messages.error(
-                request, "Username must be only letters and numbers.")
-            return redirect('register')
-
-        if password1 != password2:
-            messages.error(request, "Passwords do not match.")
-            return redirect('register')
-
-        if len(password1) < 6:
-            messages.error(request, "Password should be greater than 6.")
-            return redirect('register')
-
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Already Registered. Try logging in")
-            return redirect('register')
-
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Already Registered. Try logging in")
-            return redirect('register')
-
-        myuser = User.objects.create_user(username, email, password1)
-        myuser.save()
-        messages.success(request, "Successfully, Registered.")
-        return redirect('login')
+    states = state.objects.all()
+    districts = district.objects.all()
     if str(request.user) == 'shivansh':
-        return render(request, 'register.html')
-    return redirect('home')
-
+        if request.method == 'POST':
+            userdistrict = request.POST["district_id"]
+            email = request.POST["email"]
+            telegram = request.POST["telegram"]
+            district_id = userdistrict.split(' ')[-1].strip('()')
+            createUser = user(email=email, district_id=district_id, telegram_sub=telegram)
+            createUser.save()
+            return render(request, 'success.html', {'district':district_id, 'email':email, 'telegram':telegram})
+        return render(request, 'home.html', {'states': states, 'districts': districts})
+    return render(request, 'visitor.html',{'states': states, 'districts': districts})
 
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-
         if user is not None:
             auth_login(request, user)
-            messages.success(request, "Successfully, Logged In.")
-            return redirect('home')
+            if str(request.user) == 'shivansh':
+                messages.success(request, "Successfully, Logged In.")
+                return redirect('admin')
+            else:
+                messages.error(request, "Not Authorized to Log in, Please try again.")
+                return redirect('login')
         else:
             messages.error(request, "Invalid Credentials, Please try again.")
             return redirect('login')
     return render(request, 'login.html')
+
+@login_required
+def admin(request):
+    if str(request.user) == 'shivansh':
+        users = user.objects.all()
+        states = state.objects.all()
+        districts = district.objects.all()
+        if request.method == 'POST':
+            userdistrict = request.POST["district_id"]
+            is_active = request.POST["is_active"]
+            is_telegram = request.POST["is_telegram"]
+            telegram_channel_id = request.POST["telegram_channel_id"]
+            telegram_channel_name = request.POST["telegram_channel_name"]
+            district_id = userdistrict.split(' ')[-1].strip('()')
+            updateDistrict = district.objects.get(district_id = district_id)
+            updateDistrict.is_active = is_active
+            updateDistrict.is_telegram = is_telegram
+            updateDistrict.telegram_channel_id = telegram_channel_id
+            updateDistrict.telegram_channel_name = telegram_channel_name
+            updateDistrict.save()
+            return render(request ,'success.html', {'updateDistrict': updateDistrict})
+        return render(request, 'admin.html', {'users': users, 'states':states, 'districts': districts})
+
+
 
 def logout(request):
     auth.logout(request)
